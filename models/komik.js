56d5f395737bc81a34cd8373a4ed769eef327cb6
nachool <nachma.olivia1394@gmail.com>
2025-11-24 11:41:54 +0700

    menambahkan image

---
+++ models/komik.js
@@ -0,0 +1,29 @@
+module.exports = (sequelize, DataTypes) => {
+  const Komik = sequelize.define(
+    "Komik",
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      title: {
+        type: DataTypes.STRING,
+      },
+      description: {
+        type: DataTypes.STRING,
+      },
+      author: {
+        type: DataTypes.STRING,
+      },
+      ImageType: DataTypes.STRING,
+      ImageName: DataTypes.STRING,
+      ImageData: DataTypes.BLOB("long"),
+    },
+    {
+      tableName: "komik",
+    }
+  );
+
+  return Komik;
+};
